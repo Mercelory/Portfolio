5201045dac0c215b6915d7bd68dbb896a10df9d1
Mercelory <79084881+Mercelory@users.noreply.github.com>
2022-05-31 23:40:50 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,30 @@
+
+
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+(function () {
+    if (localStorage.getItem('theme') === 'dark_theme') {
+        setTheme('dark_theme');
+    } else {
+        setTheme('light_theme');
+    }
+ })();
+
+let changeicon = function(icon) {
+     icon.classList.toggle("uil-sun")
+        if (localStorage.getItem('theme') === 'dark_theme'){
+            setTheme('light_theme');
+        } else {
+            setTheme('dark_theme');
+        }
+     }
+
+
+
+
+// function to toggle between light and dark theme
+
+// Immediately invoked function to set the theme on initial load
