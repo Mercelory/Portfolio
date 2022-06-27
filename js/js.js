93885b83734dfc1323c6511b16220b44ce7c57e8
Mercelory <79084881+Mercelory@users.noreply.github.com>
2022-06-27 18:14:32 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -23,3 +23,10 @@ let changeicon = function(icon) {
             setTheme('dark_theme');
         }
      }
+
+
+
+
+// function to toggle between light and dark theme
+
+// Immediately invoked function to set the theme on initial load
