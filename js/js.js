cb69a5a26a8452ed23d18b5e68f6fbfbd5fde431
Mercelory <79084881+Mercelory@users.noreply.github.com>
2022-06-27 18:20:18 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -2,31 +2,3 @@ import 'animate.css';
 
 new WOW().init();
 
-function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-
-(function () {
-    if (localStorage.getItem('theme') === 'dark_theme') {
-        setTheme('dark_theme');
-    } else {
-        setTheme('light_theme');
-    }
- })();
-
-let changeicon = function(icon) {
-     icon.classList.toggle("uil-sun")
-        if (localStorage.getItem('theme') === 'dark_theme'){
-            setTheme('light_theme');
-        } else {
-            setTheme('dark_theme');
-        }
-     }
-
-
-
-
-// function to toggle between light and dark theme
-
-// Immediately invoked function to set the theme on initial load
